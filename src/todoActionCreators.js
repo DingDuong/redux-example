33d4f7fe158c6ef2940bb6f03d650fa544e1f229
Elie Schoppik <eschoppik@gmail.com>
2018-01-11 12:29:33 -0800

    cleaning up actions

--- src/todoActionCreators.js
+++ src/todoActionCreators.js
@@ -1,11 +1,11 @@
 export function addTodo(dispatch, task) {
-  dispatch({ type: "ADD_TODO", task });
+  return { type: "ADD_TODO", task };
 }
 
 export function removeTodo(dispatch, id) {
-  dispatch({ type: "REMOVE_TODO", id });
+  return { type: "REMOVE_TODO", id };
 }
 
 export function updateTodo(dispatch, task) {
-  dispatch({ type: "UPDATE_TODO", task });
+  return { type: "UPDATE_TODO", task };
 }
